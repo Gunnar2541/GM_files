f82bfdf33817b281140f507cda599043fc44acba
Gunnar M <gunnar.maemets@gmail.com>
2021-01-13 15:57:03 +0200

    ise tegin

--- src/main/java/ee/bcs/valiit/tasks/Lesson1.java
+++ src/main/java/ee/bcs/valiit/tasks/Lesson1.java
@@ -3,8 +3,8 @@ package ee.bcs.valiit.tasks;
 public class Lesson1 {
 
     public static void main(String[] args) {
-        excersie1();
-        excersie2();
+        excersie1(1,1,3);
+        excersie2(10, 20);
         excersie3();
         excersie4();
     }
@@ -14,7 +14,13 @@ public class Lesson1 {
     // Prindi välja a==b ja a==c
     // Lisa rida a = c
     // Prindi välja a==b ja a==c, mis muutus???
-    public static void excersie1(){
+    public static void excersie1(int a, int b, int c){
+        System.out.println("\nEx 1");
+        System.out.println((a==b)+" , "+(a==c));
+        a=c;
+        System.out.println((a==b)+" , "+(a==c));
+
+
     }
 
     // TODO
@@ -22,7 +28,12 @@ public class Lesson1 {
     // Tekita muutuja y1 = ++x1, trüki välja nii x1 kui y1
     // Tekita muutuja y2 = x2++, trüki välja nii x2 ja y2
     // Analüüsi tulemusi
-    public static void excersie2(){
+    public static void excersie2(int x1, int x2){
+        System.out.println("\nEx 2");
+        int y1 =++x1;
+        System.out.println(x1+" , "+y1);
+        int y2 = x2++;
+        System.out.println(x2+" , "+y2);
     }
 
     // TODO
@@ -33,12 +44,22 @@ public class Lesson1 {
     // d = 21 % 3
     // Prindi välja kõigi muutujate väärtused
     public static void excersie3(){
+        System.out.println("\nEx 3");
+        int a = 18 % 3;
+        int b = 19 % 3;
+        int c = 20 % 3;
+        int d = 21 % 3;
+        System.out.println(a+" , "+b+" , "+c+" , "+d);
     }
 
     // TODO
     // Defineeri String tüüpi muutuja mille sisu oleks "\"\\""
     // Trüki muutuja sisu välja
     public static void excersie4(){
+        System.out.println("\nEx 4");
+        String a = "  \"\\\"\\\\\"\" ";
+        System.out.println(a);
+
     }
 
 }
