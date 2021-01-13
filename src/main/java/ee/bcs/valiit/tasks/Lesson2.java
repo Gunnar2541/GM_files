91a9de76f025ed4dc04798e2df9a50211a8f08f8
Gunnar M <gunnar.maemets@gmail.com>
2021-01-13 16:18:28 +0200

    ise tegin 3h

--- src/main/java/ee/bcs/valiit/tasks/Lesson2.java
+++ src/main/java/ee/bcs/valiit/tasks/Lesson2.java
@@ -1,19 +1,49 @@
 package ee.bcs.valiit.tasks;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Lesson2 {
 
     public static void main(String[] args) {
-        // TODO siia saab kirjutada koodi testimiseks
+        System.out.println("Ex 1");
+        //exercise1(10);
+        System.out.println("\nEx 2");
+        exercise2(5);
+        System.out.println("\nEx 3");
+        exercise3(3,3);
+        System.out.println("\nEx 4");
+        System.out.println(exercise4(6));
+        System.out.println("\nEx 5");
+        System.out.println(exercise5(27));
+        System.out.println("\nEx 6");
+        System.out.println(exercise6(20, 30));
+        System.out.println("\nCodew 8");
+        System.out.println(codewars8());
+        System.out.println("\nCodew 7");
+        codewarsKata7();
+
     }
 
     // TODO loo 10 elemendile täisarvude massiv
     // TODO loe sisse konsoolist 10 täisarvu
     // TODO trüki arvud välja vastupidises järiekorras
-    public static void exercise1() {
+    public static void exercise1(int arrayItems) {
+        Scanner userNumber = new Scanner(System.in);
+        ArrayList<String> nrs = new ArrayList<>();
+        for (int u = 0; u < arrayItems; u++) {
+            System.out.print("Enter " + arrayItems + " numbers, one number at a time. You have entered " + u + " numbers so far: ");
+            String userNumberIn = userNumber.next();
+            nrs.add(u, userNumberIn);
+        }
+        System.out.println("Array as entered: " + nrs);
+
+        ArrayList<String> nrsReversed = new ArrayList<>();
+        for (int i = arrayItems - 1; i >= 0; i--) {
+            nrsReversed.add(nrs.get(i));
+        }
+        System.out.println("Array reversed: " + nrsReversed);
     }
 
     // TODO prindi välja x esimest paaris arvu
@@ -21,8 +51,10 @@ public class Lesson2 {
     // Sisend 5
     // Väljund 2 4 6 8 10
     public static void exercise2(int x) {
+        for (int i = 1; i <= x; i++) {
+            System.out.println(i * 2);
+        }
     }
-
     // TODO trüki välja korrutustabel mis on x ühikut lai ja y ühikut kõrge
     // TODO näiteks x = 3 y = 3
     // TODO väljund
@@ -37,24 +69,105 @@ public class Lesson2 {
     // TODO 5 võrdle ridu. Kas on mingi seaduspärasus ridade vahel,
     // mis on ja mis võiks olla. Äkki tuleb mõni idee
     public static void exercise3(int x, int y) {
-
+        //int[][] intMatrix = new int[x][y];
+        System.out.println("Array elements are: ");
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= y; j++) {
+                //intMatrix[i][j] = (i + 1) * (j + 1);
+                System.out.print(i*j + " ");
+            }
+            System.out.println();
+        }
     }
-
-    // TODO
+       // TODO
     // Fibonacci jada on fib(n) = fib(n-1) + fib(n-2);
     // 0, 1, 1, 2, 3, 5, 8, 13, 21
     // Tagasta fibonacci jada n element
     public static int exercise4(int n) {
-
-        return 0;
+        int i = 0;
+        int fib = 0;
+        int fib1 = 1;
+        int output = 0;
+        while (i < n - 1) {
+            output = fib + fib1;
+            fib1 = fib;
+            fib = output;
+            i++;
+        }
+        return output;
     }
 
-    public static void exercise5() {
+    public static int exercise5(int n) {
+
         // https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=36
         // TODO 1 (tee alamfunktsioon) mis leiab 3n+1 sequenci pikkuse
-        // kui on paaris / 2 kui on paaritu *3+1
-        // TODO 2 tee tsükkel mis leiab i -> j kõige suurema tsükkli pikkuse
+        //int n = 24;
+        int counter = 0;
+        while (n != 1) {
+            counter++;
+            //System.out.println("Check " + counter + " result " + n);
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = (3 * n) + 1;
+            }
+        }
+        //System.out.println("Check " + (counter + 1) + " result " + n);
+        return (counter + 1);
+    }
+            /*
+            1. input n
+            2. print n
+            3. if n = 1 then STOP
+            4. if n is odd then n ←− 3n + 1
+            5. else n ←− n/2
+            6. GOTO 2
+                         */
+
+    // kui on paaris / 2 kui on paaritu *3+1
+    // TODO 2 tee tsükkel mis leiab i -> j kõige suurema tsükkli pikkuse
+    public static int exercise6(int g, int c) {
+        int outcome = 0;
+        for (int i = g; i <= c; i++) {
+            if (exercise5(i) > outcome)
+                outcome = i;
+        }
+        return outcome;
+    }
+    /*
+    You are given an array with positive numbers and a non-negative number N.
+    You should find the N-th power of the element in the array with the index N.
+    If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+    Let's look at a few examples:
+    array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
+    array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
+    */
+    public static int codewars8 () {
+        int[] nrs = {1,2,3,4};
+        int indx =3;
+        if (nrs.length<=indx){
+            return -1;
+        }else {
+            return ((int) Math.pow(nrs[indx], indx));
+        }
+    }
+    //Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+    //Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+    public static void codewarsKata7 () {
+        String words = "How can mirrors be real if our eyes aren't real";
+        String[] newWords = words.split(" ");
+        ArrayList<String> newString = new ArrayList<>();
+        for (int i = 0; i < newWords.length; i++) {
+            //System.out.println(newWords[i]);
+            String s1 = newWords[i].substring(0, 1).toUpperCase();
+            String nameCapitalized = s1 + newWords[i].substring(1);
+            newString.add(nameCapitalized);
+        }
+        //System.out.println(newString);
+        String convertedString = String.join(" ", newString);
+        System.out.println(convertedString);
+        }
+
     }
 
 
-}
