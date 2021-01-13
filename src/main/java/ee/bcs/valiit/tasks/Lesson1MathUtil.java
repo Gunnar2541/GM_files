0ac2d932faf813ed6096a277bbbc1183edad7a53
Gunnar M <gunnar.maemets@gmail.com>
2021-01-13 16:19:25 +0200

    isetegin1

--- src/main/java/ee/bcs/valiit/tasks/Lesson1MathUtil.java
+++ src/main/java/ee/bcs/valiit/tasks/Lesson1MathUtil.java
@@ -9,36 +9,66 @@ public class Lesson1MathUtil {
     }
 
     public static void main(String[] args) {
+        System.out.println(min(3,10));
+        System.out.println(max(3,10));
+        System.out.println(abs(-18));
+        System.out.println(isEven(5));
+        System.out.println(isEven(4));
+        System.out.println(min(3,3, 6));
+        System.out.println(max(4,4,8));
+
     }
 
     // TODO tagasta a ja b väikseim väärtus
     public static int min(int a, int b) {
-        return 0;
+        if (a<b)
+            return a;
+        else
+            return b;
     }
 
     // TODO tagasta a ja b suurim väärtus
     public static int max(int a, int b) {
-        return 0;
+        if (a>b)
+            return a;
+        else
+            return b;
     }
 
     // TODO tagasta a absoluut arv
     public static int abs(int a) {
-        return 0;
+        if(a>=0)
+            return a;
+        else
+            return (a*-1);
     }
 
     // TODO tagasta true, kui a on paaris arv
     // tagasta false kui a on paaritu arv
     public static boolean isEven(int a) {
-        return true;
+        if(a%2==0)
+            return true;
+        else
+            return false;
     }
 
     // TODO tagasta kolmest arvust kõige väiksem
     public static int min(int a, int b, int c) {
-        return 0;
+        if (b<=c&&b<=a)
+            return b;
+        if (a<=c&&a<=b)
+            return a;
+        else
+            return c;
     }
 
     // TODO tagasta kolmest arvust kõige suurem
     public static int max(int a, int b, int c) {
-        return 0;
+        if (b>=c&&b>=a)
+            return b;
+        if (a>=c&&a>=b)
+            return a;
+        else
+            return c;
     }
 }
