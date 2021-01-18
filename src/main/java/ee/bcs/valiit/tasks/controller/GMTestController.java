80c9febb7ef2e6a675562e761db1ad3926cac812
Gunnar M <gunnar.maemets@gmail.com>
2021-01-18 15:33:50 +0200

    Controller

--- src/main/java/ee/bcs/valiit/tasks/controller/GMTestController.java
+++ src/main/java/ee/bcs/valiit/tasks/controller/GMTestController.java
@@ -1,9 +1,10 @@
 package ee.bcs.valiit.tasks.controller;
 
 import ee.bcs.valiit.tasks.Lesson1MathUtil;
+import ee.bcs.valiit.tasks.solution.SolutionLesson1MathUtil;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("solution/lesson")
+@RequestMapping("solution")
 @RestController
 public class GMTestController {
 
@@ -11,15 +12,15 @@ public class GMTestController {
     public String getHelloWorld() {
         return "Hello World";
     }
-
+    //http://localhost:8080/solution/Lesmin/3/9
     @GetMapping("Lesmin/{a}/{b}")
     public int min(@PathVariable("a") int a, @PathVariable("b") int b) {
         return Lesson1MathUtil.min(a, b);
     }
-
-    @GetMapping("Lesmax/{a}/{b}")
-    public int max(@PathVariable("a") int a, @PathVariable("b") int b) {
-        return Lesson1MathUtil.max(a, b);
+    //http://localhost:8080/solution/Lesmax?a=3&b=9
+    @GetMapping("Lesmax")
+    public int max(@RequestParam("a") int aVariable, @RequestParam("b") int bVariable) {
+        return Lesson1MathUtil.max(aVariable, bVariable);
     }
 
     @GetMapping("LesisEven/{a}")
